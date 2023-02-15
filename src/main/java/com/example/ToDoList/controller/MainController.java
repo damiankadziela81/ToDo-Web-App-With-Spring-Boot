9db4de4fdac9fe53a8839769f519d0e3e191b3c4
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-15 11:12:42 +0100

    Init commit - Controller and View implemented

---
+++ src/main/java/com/example/ToDoList/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.ToDoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String getTasks(){
+        return "index";
+    }
+}
