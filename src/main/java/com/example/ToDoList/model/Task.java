4d8b37172d0e25ffab0e2b6d7d2b5f1c6d9f352b
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-15 12:15:02 +0100

    Added task entity

---
+++ src/main/java/com/example/ToDoList/model/Task.java
@@ -0,0 +1,56 @@
+package com.example.ToDoList.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private Integer id;
+    private Integer priority;
+    private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    public Task() {
+    }
+
+    public Task(final Integer id, final Integer priority, final String description, final LocalDate date) {
+        this.id = id;
+        this.priority = priority;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(final Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(final LocalDate date) {
+        this.date = date;
+    }
+}
