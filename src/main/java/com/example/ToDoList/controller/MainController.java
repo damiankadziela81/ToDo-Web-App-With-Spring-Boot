4a5b0f5a309673415c1f1414ed989fbd02b2b868
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-16 14:33:46 +0100

    Added "Done" links for task removal

--- src/main/java/com/example/ToDoList/controller/MainController.java
+++ src/main/java/com/example/ToDoList/controller/MainController.java
@@ -5,6 +5,7 @@ import com.example.ToDoList.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,16 +19,14 @@ public class MainController {
     @RequestMapping("/")
     public String getTasks(Model model){
 
-//        List<Task> tasks = new ArrayList<>();
-//        tasks.add(new Task(1,2,"pay bills", LocalDate.of(2023,3,1)));
-//        tasks.add(new Task(2,3,"call John", LocalDate.of(2023,2,21)));
-//        tasks.add(new Task(3,1,"add new feature in my app", LocalDate.of(2023,2,15)));
-//        tasks.add(new Task(4,2,"prepare for presentation", LocalDate.of(2023,3,5)));
-
         List<Task> tasks = taskRepository.findAll();
-
         model.addAttribute("tasks",tasks);
-
         return "index";
+    }
+
+    @RequestMapping("/tasks/{id}")
+    public String closeTask(@PathVariable int id){
+        taskRepository.deleteById(id);
+        return "redirect:/";
     }
 }
