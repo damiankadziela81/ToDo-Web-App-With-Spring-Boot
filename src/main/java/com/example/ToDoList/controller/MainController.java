14363f684cbab798991606b581fc4d13458d5d66
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-16 15:28:23 +0100

    Implemented "add new task" feature

--- src/main/java/com/example/ToDoList/controller/MainController.java
+++ src/main/java/com/example/ToDoList/controller/MainController.java
@@ -5,7 +5,9 @@ import com.example.ToDoList.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,6 +29,17 @@ public class MainController {
     @RequestMapping("/tasks/{id}")
     public String closeTask(@PathVariable int id){
         taskRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/newTask")
+    public String getTaskForm(){
+        return "task-form";
+    }
+
+    @PostMapping("/processForm")
+    public String addTask(@ModelAttribute Task task){
+        taskRepository.save(task);
         return "redirect:/";
     }
 }
