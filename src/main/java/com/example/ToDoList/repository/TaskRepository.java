723917063afe01cb58df4238420397167879bdbb
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-16 13:07:52 +0100

    Added repository

---
+++ src/main/java/com/example/ToDoList/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.ToDoList.repository;
+
+import com.example.ToDoList.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Integer> {
+}
